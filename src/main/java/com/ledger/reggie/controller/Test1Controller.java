906e743b1a12b93ad95730ce86fd76961a96579a
ledger <228664584@qq.com>
2023-04-17 10:28:24 +0800

    test

--- src/main/java/com/ledger/reggie/controller/Test1Controller.java
+++ src/main/java/com/ledger/reggie/controller/Test1Controller.java
@@ -1,8 +1,11 @@
 package com.ledger.reggie.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author ledger
  * @version 1.0
  **/
+@RestController
 public class Test1Controller {
 }
